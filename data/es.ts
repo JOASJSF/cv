49f22502723d4b66128c29c5641417c73a63c7d1
Joaquín Martínez <55899778+JOASJSF@users.noreply.github.com>
2024-09-08 23:51:29 -0300

    Update es.ts
    emojis add

--- data/es.ts
+++ data/es.ts
@@ -33,7 +33,7 @@ const data: Data = {
       start: '',
       end: '',
       description:
-        'Se tomó la decisión de usar Astro y Tailwind CSS para obtener la flexibilidad y rapidez que proporcionan estas tecnologías en contraste con otras (WordPress). Con esto se lograron muy buenos resultados en https://pagespeed.web.dev/ \n(99 Rendimiento, 91 Accesibilidad, 99 Prácticas Recomendadas y 99 SEO).\n\n• Compra de dominio en nic.ar. \n• Desarrollo de la página. \n• Instalación de Ubuntu 20.04 en VPS junto con Coolify(mediante terminal) para el despliegue de la página.',
+        'Se tomó la decisión de usar Astro y Tailwind CSS para obtener la flexibilidad y rapidez que proporcionan estas tecnologías en contraste con otras (WordPress). Con esto se lograron muy buenos resultados en https://pagespeed.web.dev/ \n(99 Rendimiento, 91 Accesibilidad, 99 Prácticas Recomendadas y 99 SEO).\n\n✅ Compra de dominio en nic.ar. \n✅ Desarrollo de la página. \n✅ Instalación de Ubuntu 20.04 en VPS junto con Coolify(mediante terminal) para el despliegue de la página.',
     },
     {
       company: 'Panel administrador y apps moviles',
@@ -42,7 +42,7 @@ const data: Data = {
       title: 'Implementación de nuevas características',
       start: '',
       end: '',
-      description: `Un proyecto existente constaba de un panel administrador y una API desarrollados en Laravel, junto con dos aplicaciones móviles: una para clientes y otra para comercios, desarrolladas en Flutter, utilizando la pasarela de pagos de MercadoPago y una base de datos compartida en MySQL. El funcionamiento de este proyecto era similar a plataformas como Rappi o PedidosYa: cuando un cliente realizaba una compra en un comercio y efectuaba el pago, el dinero se transfería a una única cuenta de MercadoPago (la del administrador). Los comercios debían solicitar al administrador retiros de sus ventas, y este realizaba manualmente las transferencias a cada comercio que lo solicitaba.\n\n Con esta premisa, se me contrató para modificar la forma en que el proyecto gestionaba los cobros. El requerimiento consistía en que, al realizar una compra, el pago se dividiera automáticamente entre la comisión del administrador y las ganancias del comercio. Para lograr esto, ya que el proyecto utilizaba la pasarela de pagos de MercadoPago, se implementó una solución que ellos mismos ofrecen, conocida como "Split de pagos", que cuenta con escasa documentación.\n\n •Se modificó la aplicación de los comercios para integrar el proceso de autorización OAuth (requerido por MercadoLibre para procesar este tipo de pagos). \n•Se crearon dos endpoints en la API: uno para guardar encriptados los tokens obtenidos de los comercios al realizar el OAuth, y otro para verificar periódicamente si el comercio poseía los tokens de autorización, permitiendo o no la realización de ventas. \n•Se modificó el cURL existente que realizaba el POST a MercadoPago, de modo que ahora dividiera los pagos como se requería. \n•Se realizaron modificaciones adicionales en el panel administrativo para mostrar qué comercios estaban habilitados para realizar ventas.`,
+      description: `Un proyecto existente constaba de un panel administrador y una API desarrollados en Laravel, junto con dos aplicaciones móviles: una para clientes y otra para comercios, desarrolladas en Flutter, utilizando la pasarela de pagos de MercadoPago y una base de datos compartida en MySQL. El funcionamiento de este proyecto era similar a plataformas como Rappi o PedidosYa: cuando un cliente realizaba una compra en un comercio y efectuaba el pago, el dinero se transfería a una única cuenta de MercadoPago (la del administrador). Los comercios debían solicitar al administrador retiros de sus ventas, y este realizaba manualmente las transferencias a cada comercio que lo solicitaba.\n\n Con esta premisa, se me contrató para modificar la forma en que el proyecto gestionaba los cobros. El requerimiento consistía en que, al realizar una compra, el pago se dividiera automáticamente entre la comisión del administrador y las ganancias del comercio. Para lograr esto, ya que el proyecto utilizaba la pasarela de pagos de MercadoPago, se implementó una solución que ellos mismos ofrecen, conocida como "Split de pagos", que cuenta con escasa documentación.\n\n ✅Se modificó la aplicación de los comercios para integrar el proceso de autorización OAuth (requerido por MercadoLibre para procesar este tipo de pagos). \n✅Se crearon dos endpoints en la API: uno para guardar encriptados los tokens obtenidos de los comercios al realizar el OAuth, y otro para verificar periódicamente si el comercio poseía los tokens de autorización, permitiendo o no la realización de ventas. \n✅Se modificó el cURL existente que realizaba el POST a MercadoPago, de modo que ahora dividiera los pagos como se requería. \n✅Se realizaron modificaciones adicionales en el panel administrativo para mostrar qué comercios estaban habilitados para realizar ventas.`,
     },
     {
       company: 'Aplicación para Android',
@@ -52,7 +52,7 @@ const data: Data = {
       start: '',
       end: '',
       description:
-        'Desarrollo sencillo para adquirir experiencia en el uso y depliegue con la Android Developers Console.',
+        '✅Desarrollo sencillo para adquirir experiencia en el uso y depliegue con la Android Developers Console.',
     },
   ],
   contacts: [
